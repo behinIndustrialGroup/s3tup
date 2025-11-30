f15e380bb6416f0b4a143d443da7d97184409147
behinIndustrialGroup <info@behin.energy>
2025-11-30 14:23:52 +0330

    Update FileField.php

--- packages/behin-form-builder/src/Fields/FileField.php
+++ packages/behin-form-builder/src/Fields/FileField.php
@@ -9,39 +9,37 @@ class FileField extends AbstractField
         $s = '<div class="form-group">';
         $s .= '<label>';
         $s .= trans('fields.' . $this->name);
-        if($this->attributes['required'] == 'on' && $this->attributes['readonly'] != 'on'){
+        if ($this->attributes['required'] == 'on' && $this->attributes['readonly'] != 'on') {
             $s .= ' <span class="text-danger">*</span>';
         }
         $s .= '</label><br>';
-        if(isset($this->attributes['value']) && is_string($this->attributes['value'])){
+        if (isset($this->attributes['value']) && is_string($this->attributes['value'])) {
             $s .= '<a href="' . url('public/' . $this->attributes['value']) . '" target="_blank" download>' . trans('fields.Download') . '</a><br>';
         }
-        if(isset($this->attributes['value']) && is_array($this->attributes['value']) && count($this->attributes['value']) > 0){
-            foreach($this->attributes['value'] as $value){
+        if (isset($this->attributes['value']) && is_array($this->attributes['value']) && count($this->attributes['value']) > 0) {
+            foreach ($this->attributes['value'] as $value) {
                 $s .= '<a href="' . url('public/' . $value) . '" target="_blank" download>' . trans('fields.Download') . '</a><br>';
             }
         }
-        if($this->attributes['readonly'] == 'on'){
+        if ($this->attributes['readonly'] == 'on') {
             $s .= '<input type="file" multiple name="' . $this->name . '" disabled>';
-        }else{
+        } else {
             $s .= '<input type="file" name="' . $this->name . '" ';
-            foreach($this->attributes as $key => $value){
-                if($key != 'value'){
-                    if($key == 'required'){
-                        if($value == 'on'){
-                            $s .= 'required ';
-                        }
+            foreach ($this->attributes as $key => $value) {
+                if ($key == 'required') {
+                    if ($value == 'on') {
+                        $s .= 'required ';
                     }
-                    elseif($key == 'readonly'){
-                        if($value == 'on'){
-                            $s .= 'readonly ';
-                        }
-                    }else{
-                        $s .= $key . '="' . $value . '" ';
-                    }
+                } else {
+                    $s .= $key . '="' . $value . '" ';
                 }
             }
             $s .= '>';
+            if (isset($this->attributes['script'])) {
+                $s .= '<script>';
+                $s .= $this->attributes['script'];
+                $s .= '</script>';
+            }
         }
         $s .= '</div>';
         return $s;
