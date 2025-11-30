92c65b5f0e648572d5d061e0bab196dbd17681b3
behinIndustrialGroup <info@behin.energy>
2025-11-30 15:05:28 +0330

    Update file_control.php

--- config/file_control.php
+++ config/file_control.php
@@ -1,5 +1,5 @@
 <?php
 
 return[
-    'valid_file_type' => ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf', 'video/mp4', 'video/mpeg', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
+    'valid_file_type' => ['image/jpg', 'image/jpeg', 'application/zip', 'application/vnd.rar', 'image/png', 'application/pdf', 'video/mp4', 'video/mpeg', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
 ];
