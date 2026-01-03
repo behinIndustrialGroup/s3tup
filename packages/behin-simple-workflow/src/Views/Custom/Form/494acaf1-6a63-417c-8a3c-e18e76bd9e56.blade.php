02e301acd4d0a4a2e4f75e042dfb67a47a8f1ec4
behinIndustrialGroup <info@behin.energy>
2026-01-03 14:31:05 +0330

    Update 494acaf1-6a63-417c-8a3c-e18e76bd9e56.blade.php

--- packages/behin-simple-workflow/src/Views/Custom/Form/494acaf1-6a63-417c-8a3c-e18e76bd9e56.blade.php
+++ packages/behin-simple-workflow/src/Views/Custom/Form/494acaf1-6a63-417c-8a3c-e18e76bd9e56.blade.php
@@ -1,27 +1,37 @@
-
 @php
-use Behin\SimpleWorkflow\Models\Entities\Sales;
-use Behin\SimpleWorkflow\Models\Entities\Sale_items;
-use Behin\SimpleWorkflow\Models\Entities\Zarinpal_payment_records;
+    use Behin\SimpleWorkflow\Models\Entities\Sales;
+    use Behin\SimpleWorkflow\Models\Entities\Sale_items;
+    use Behin\SimpleWorkflow\Models\Entities\Zarinpal_payment_records;
+    use Behin\SimpleWorkflow\Models\Entities\Azkivam_payment_records;
     $sale = Sales::where('case_number', $case->number)->first();
     $saleItems = Sale_items::where('case_number', $case->number)->get();
     $s3tupPayments = Zarinpal_payment_records::where('case_number', $case->number)->get();
+    $azkiPayments = Azkivam_payment_records::where('case_number', $case->number)->get();
 @endphp
 
-
-<table>
-    <tr>
-        <th>شرح پرداخت</th>
-        <th>مبلغ کل</th>
-        <th>درگاه</th>
-        <th>پرداخت</th>
-    </tr>
-    @foreach ($s3tupPayments as $item)
+<div class="table-responsive">
+    <table class="table table-striped">
         <tr>
-            <td>{{ $item->description }}</td>    
-            <td>{{ $item->total }}</td>    
-            <td>{{ $item->payment_gateway }}</td>    
-            <td>{{ $item->payment_status }}</td>    
-        </tr>    
-    @endforeach
-</table>
+            <th>شرح پرداخت</th>
+            <th>مبلغ کل</th>
+            <th>درگاه</th>
+            <th>پرداخت</th>
+        </tr>
+        @foreach ($s3tupPayments as $item)
+            <tr>
+                <td>{{ $item->description }}</td>
+                <td>{{ $item->amount }}</td>
+                <td>درگاه ستاپ</td>
+                <td>{{ $item->status }}</td>
+            </tr>
+        @endforeach
+        @foreach ($azkiPayments as $item)
+            <tr>
+                <td>{{ $item->description }}</td>
+                <td>{{ $item->amount }}</td>
+                <td>درگاه ازکی وام</td>
+                <td>{{ $item->status }}</td>
+            </tr>
+        @endforeach
+    </table>
+</div>
